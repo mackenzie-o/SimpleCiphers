800916ad0086abb8eb5b8b6cf87d813d3b41a7ff
mackenzie-o <mackenzie.obleness@gmail.com>
2015-07-18 12:41:03 -0600

    comment railfence cipher

--- app/src/main/java/com/mackenzie_o/simpleciphers/ciphers/RailFence.java
+++ app/src/main/java/com/mackenzie_o/simpleciphers/ciphers/RailFence.java
@@ -1,7 +1,16 @@
 package com.mackenzie_o.simpleciphers.ciphers;
 
 /**
- * TODO: comment
+ *  The RailFence class implements the rail fence (aka zigzag) transposition cipher.
+ *
+ *  The rail fence cipher encodes messages by taking a message and writing each letter
+ *  on a "rail" or line. For example "FenceDiagram" written on three rails would look
+ *  like this:
+ *  F ~ ~ ~ e ~ ~ ~ g ~ ~ ~
+ *  ~ e ~ c ~ D ~ a ~ r ~ m
+ *  ~ ~ n ~ ~ ~ i ~ ~ ~ a ~
+ *  The message is then read from left to right, top to bottom, resulting in the cipher
+ *  text "FegecDarmnia".
  */
 public class RailFence extends Ciphers{
     String plaintext;
@@ -9,6 +18,8 @@ public class RailFence extends Ciphers{
     int rails;
     char[][] fence;
 
+    // Creates a rail fence object with the fence initalized from either the cipher or plain text
+    // depending on encode flag.
     public RailFence(String message, int key, boolean persistCaps, boolean keepCharacters, boolean encode) {
         if (!persistCaps) {
             message = message.toLowerCase();
@@ -27,6 +38,7 @@ public class RailFence extends Ciphers{
 
     }
 
+    // returns a string representing the fence with ~ representing a null character.
     public String railFenceDiagram() {
         String out = "";
         for (int i = 0; i < rails; i++) {
@@ -39,6 +51,7 @@ public class RailFence extends Ciphers{
         return out;
     }
 
+    // Creates a fence array
     private void buildFence(int length, String text) {
         fence = new char[rails][length];
         char[] m = text.toCharArray();
@@ -65,10 +78,13 @@ public class RailFence extends Ciphers{
         }
     }
 
+    // builds a fence from the plaintext
     private void buildFenceEncode() {
         buildFence(plaintext.length(), plaintext);
     }
 
+    // builds a fence from the cipher text by building a plaintext fence of the same length
+    // with placeholder characters, and then replacing those characters with the ciphertext
     private void buildFenceDecode() {
         String placeholder = "";
         char phChar = 'X';
@@ -89,6 +105,7 @@ public class RailFence extends Ciphers{
         }
     }
 
+    // returns the plaintext of the fence by reading top to bottom, left to right
     public String getPlaintext() {
         if (plaintext != null) {
             return plaintext;
@@ -102,6 +119,7 @@ public class RailFence extends Ciphers{
         return plaintext;
     }
 
+    // returns the ciphertext of the fence by reading left to right, top to bottom
     public String getCiphertext() {
         if (ciphertext != null) {
             return ciphertext;
@@ -116,6 +134,7 @@ public class RailFence extends Ciphers{
         return ciphertext;
     }
 
+    // prints out the fence to standard out for debugging
     public void printFence() {
         for (int i = 0; i < rails; i++) {
             for (int j = 0; j < fence[i].length; j++) {
